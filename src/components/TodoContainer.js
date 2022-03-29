2a366ede06341c9d3e4e50b99c9509ab4120f316
KABIKA681 <pascalkabikampan@gmail.com>
2022-03-29 15:57:21 +0200

    Change class based to function based components

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,33 @@
 import React from "react"
-class TodoContainer extends React.Component {
-  render() {
+import Header from "./Header";
+import TodosList from './TodosList'
+const TodoContainer = () => {
+    const state = {
+        todos: [
+          {
+            id: 1,
+            title: "Setup development environment",
+            completed: true
+          },
+          {
+            id: 2,
+            title: "Develop website and add content",
+            completed: false
+          },
+          {
+            id: 3,
+            title: "Deploy to live server",
+            completed: false
+          }
+        ]
+       };
+
     return (
-      <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
+        <div>
+            <Header/>
+        <TodosList todos={this.state.todos} />
       </div>
     )
   }
-}
-export default TodoContainer
+
+export default TodoContainer;
