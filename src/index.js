4e05a094541e21795fa45081b6bb68145ddcd8d7
KABIKA681 <pascalkabikampan@gmail.com>
2022-03-29 15:24:03 +0200

    Add Todocontainer logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TodoContainer from './components/TodoContainer'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <TodoContainer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
